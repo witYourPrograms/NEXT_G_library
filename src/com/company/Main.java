0b190e5af69d16e2077588cf79cbe14e290b1724
Cezary Kania <czainaktv@gmail.com>
2018-01-12 09:37:12 +0100

    On branch master Your branch is up to date with 'origin/master'. Changes to be committed: 	new file:   .idea/description.html 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/project-template.xml 	new file:   .idea/vcs.xml 	new file:   .idea/workspace.xml 	new file:   NEXT_G_library.iml 	new file:   out/production/NEXT_G_library/com/company/Main.class 	new file:   out/production/NEXT_G_library/com/company/User.class 	new file:   src/com/company/Main.java 	new file:   src/com/company/User.java

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        User Cezary = new User();
+        Cezary.disp("test");
+    }
+}
---
+++ src/com/company/User.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class User {
+    public void disp(String test)
+    {
+        System.out.println(test);
+    }
+}
